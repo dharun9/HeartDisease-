31c8e82e69cf0237b59c1aa1e5e733b72d667a97
unknown <dharunjagan009@gmail.com>
2023-09-03 11:11:38 +0530

    StreamLit

--- app.py
+++ app.py
@@ -1,41 +1,66 @@
 import streamlit as st
 import pickle
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
-st.title("Heart Disease Recognition")
+# Set page title
+st.title("Heart Disease Prediction")
 
-# Add input fields for the specified features
-sex = st.number_input("Insert the Sex (0 for female, 1 for male)")
-cp = st.number_input("Insert the Chest Pain Type")
-trestbps = st.number_input("Insert the Resting Blood Pressure")
-chol = st.number_input("Insert the Cholesterol Level")
-fbs = st.number_input("Insert the Fasting Blood Sugar (0 for False, 1 for True)")
-restecg = st.number_input("Insert the Resting ECG")
-thalach = st.number_input("Insert the Max Heart Rate")
-exang = st.number_input("Insert Exercise-Induced Angina (0 for No, 1 for Yes)")
-oldpeak = st.number_input("Insert the Oldpeak")
-slope = st.number_input("Insert the Slope")
-ca = st.number_input("Insert the Number of Major Vessels (0-3)")
-thal = st.number_input("Insert the Thalassemia Type")
+# Load the trained KNN model
+try:
+    with open("knn_model.pkl", "rb") as f:
+        knn_model = pickle.load(f)
+except FileNotFoundError:
+    st.error("Model file 'knn_model.pkl' not found. Please make sure it exists in the correct location.")
+except Exception as e:
+    st.error(f"An error occurred while loading the model: {str(e)}")
 
-# Provide a default value for the missing feature
-missing_feature = 0  # You can change this to an appropriate default value
-
-submit = st.button("Predict")
-if submit:
-    try:
-        with open("knn_model.pkl", "rb") as f:
-            knn_model = pickle.load(f)
-    except FileNotFoundError:
-        st.error("Model file 'knn_model.pkl' not found. Please make sure it exists in the correct location.")
-    except Exception as e:
-        st.error(f"An error occurred while loading the model: {str(e)}")
-
-    # Prepare the input data with the missing feature
-    input_data = [[sex, cp, trestbps, chol, fbs, restecg, thalach, exang, oldpeak, slope, ca, thal, missing_feature]]
-
+# Function to make predictions
+def predict_heart_disease(sex, cp, trestbps, chol, fbs, restecg, thalach, exang, oldpeak, slope, ca, thal, missing_feature):
+    # Prepare the input data
+    input_data = np.array([[sex, cp, trestbps, chol, fbs, restecg, thalach, exang, oldpeak, slope, ca, thal, missing_feature]])
+    
     # Predict heart disease
-    try:
-        output = knn_model.predict(input_data)
-        st.success("Predicted Heart Disease (0 for No, 1 for Yes): " + str(output[0]))
-    except Exception as e:
-        st.error(f"An error occurred while making predictions: {str(e)}")
+    prediction = knn_model.predict(input_data)
+    
+    return prediction[0]
+
+# Add input fields for user input
+sex = st.number_input("Sex (0 for female, 1 for male)", min_value=0, max_value=1)
+cp = st.number_input("Chest Pain Type", min_value=0)
+trestbps = st.number_input("Resting Blood Pressure")
+chol = st.number_input("Cholesterol Level")
+fbs = st.number_input("Fasting Blood Sugar (0 for False, 1 for True)", min_value=0, max_value=1)
+restecg = st.number_input("Resting ECG")
+thalach = st.number_input("Max Heart Rate")
+exang = st.number_input("Exercise-Induced Angina (0 for No, 1 for Yes)", min_value=0, max_value=1)
+oldpeak = st.number_input("Oldpeak")
+slope = st.number_input("Slope")
+ca = st.number_input("Number of Major Vessels (0-3)", min_value=0, max_value=3)
+thal = st.number_input("Thalassemia Type")
+
+# Default value for the missing feature
+missing_feature = st.number_input("Missing Feature (e.g., 0)", min_value=0)
+
+# Make prediction when the user clicks the button
+if st.button("Predict Heart Disease"):
+    # Predict using the model
+    prediction = predict_heart_disease(sex, cp, trestbps, chol, fbs, restecg, thalach, exang, oldpeak, slope, ca, thal, missing_feature)
+
+    # Display the prediction result
+    if prediction == 1:
+        st.error("Prediction: Heart Disease (1)")
+    else:
+        st.success("Prediction: No Heart Disease (0)")
+
+    # Create a bar chart to visualize input values
+    input_values = [sex, cp, trestbps, chol, fbs, restecg, thalach, exang, oldpeak, slope, ca, thal, missing_feature]
+    input_labels = ["Sex", "Chest Pain", "Resting BP", "Cholesterol", "Fasting BS", "Resting ECG", "Max HR", "Exang", "Oldpeak", "Slope", "Major Vessels", "Thalassemia", "Missing Feature"]
+    
+    st.header("Input Values Visualization")
+    plt.figure(figsize=(10, 6))
+    plt.barh(input_labels, input_values, color='skyblue')
+    plt.xlabel("Value")
+    plt.title("Input Values")
+    st.pyplot()
